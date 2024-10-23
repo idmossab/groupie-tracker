657f5aeca30005f4d528f0cbd96f36fea0c3c17e
midbenke <idmossab@gmail.com>
2024-10-23 22:56:46 +0100

    fetch data v_3

--- jsonReader.go
+++ jsonReader.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func fetchArtist(url string) Artist {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var artist Artist
+	err = json.Unmarshal(body, &artist)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return artist
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -9,20 +10,24 @@ import (
 )
 
 func main() {
-	
-	byteValue,err :=os.ReadFile("users.json")
-	if err!=nil{
-		log.Fatal("Error reading Json file :",err)
+	byteValue, err := os.ReadFile("users.json")
+	if err != nil {
+		log.Fatal("Error reading Json file :", err)
 	}
 	var users []User
-	//fech data json to struct
-	json.Unmarshal(byteValue,&users)
+	// fech data json to struct
+	json.Unmarshal(byteValue, &users)
 
-	temp :=template.Must(template.ParseFiles("index.html"))
+	temp := template.Must(template.ParseFiles("index.html"))
 
-	http.HandleFunc("/",func (w http.ResponseWriter,r *http.Request){
-		temp.Execute(w,users)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		temp.Execute(w, users)
 	})
 
-	http.ListenAndServe(":8080",nil)
+	// Fetch artist data
+	artistURL := "https://groupietrackers.herokuapp.com/api/artists/1"
+	artist := fetchArtist(artistURL)
+	fmt.Printf("Artist: %+v\n", artist)
+
+	http.ListenAndServe(":8080", nil)
 }
