8f2a64585784a45c6a8f4cd1aa2e5b0987e75548
midbenke <idmossab@gmail.com>
2024-10-23 20:55:46 +0100

    fetch data v_1

---
+++ fetchData.go
@@ -0,0 +1,28 @@
+package main
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	LocationsURL string   `json:"locations"` // The URL for locations data
+
+}
+
+type Locations struct {
+	ID        int      `json:"id"`
+	Locations []string `json:"locations"`
+}
+
+type Dates struct {
+	ID    int      `json:"id"`
+	Dates []string `json:"dates"`
+}
+
+type Relation struct {
+	ID        int      `json:"id"`
+	Dates     []string `json:"dates"`
+	Locations []string `json:"locations"`
+}
