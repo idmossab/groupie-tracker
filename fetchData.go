3674593c54a9253b365d0f616a6ddf5a647a0ed3
midbenke <idmossab@gmail.com>
2024-10-23 21:23:13 +0100

    first test fetch and showdata

--- fetchData.go
+++ fetchData.go
@@ -1,28 +1,7 @@
 package main
 
-type Artist struct {
-	ID           int      `json:"id"`
-	Image        string   `json:"image"`
-	Name         string   `json:"name"`
-	Members      []string `json:"members"`
-	CreationDate int      `json:"creationDate"`
-	FirstAlbum   string   `json:"firstAlbum"`
-	LocationsURL string   `json:"locations"` // The URL for locations data
-
-}
-
-type Locations struct {
-	ID        int      `json:"id"`
-	Locations []string `json:"locations"`
-}
-
-type Dates struct {
-	ID    int      `json:"id"`
-	Dates []string `json:"dates"`
-}
-
-type Relation struct {
-	ID        int      `json:"id"`
-	Dates     []string `json:"dates"`
-	Locations []string `json:"locations"`
+type User struct {
+    Name  string `json:"name"`
+    Email string `json:"email"`
+    Age   int    `json:"age"`
 }
--- main.go
+++ main.go
@@ -2,62 +2,27 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	// Step 1: Fetch data from the artists API
-	url := "https://groupietrackers.herokuapp.com/api/artists"
-	response, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+	
+	byteValue,err :=os.ReadFile("users.json")
+	if err!=nil{
+		log.Fatal("Error reading Json file :",err)
 	}
-	defer response.Body.Close()
+	var users []User
+	//fech data json to struct
+	json.Unmarshal(byteValue,&users)
 
-	// Step 2: Read and parse the JSON response for artists
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	temp :=template.Must(template.ParseFiles("index.html"))
 
-	var artists []Artist
-	err = json.Unmarshal(body, &artists)
-	if err != nil {
-		log.Fatal(err)
-	}
+	http.HandleFunc("/",func (w http.ResponseWriter,r *http.Request){
+		temp.Execute(w,users)
+	})
 
-	// Step 3: For each artist, fetch the locations data and display it
-	for _, artist := range artists {
-		fmt.Printf("Artist ID: %d\n", artist.ID)
-		fmt.Printf("Name: %s\n", artist.Name)
-		fmt.Printf("Members: %v\n", artist.Members)
-		fmt.Printf("Creation Date: %d\n", artist.CreationDate)
-		fmt.Printf("First Album: %s\n", artist.FirstAlbum)
-		fmt.Printf("Image URL: %s\n", artist.Image)
-
-		// Fetch the locations data using the LocationsURL
-		locationResponse, err := http.Get(artist.LocationsURL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer locationResponse.Body.Close()
-
-		locationBody, err := ioutil.ReadAll(locationResponse.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var locationData Locations
-		err = json.Unmarshal(locationBody, &locationData)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Display the locations for the artist
-		fmt.Printf("Locations: %v\n", locationData.Locations)
-		fmt.Println("----------------------------")
-	}
+	http.ListenAndServe(":8080",nil)
 }
