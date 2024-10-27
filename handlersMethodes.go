5d05ef520d547ea3d99d0985736f68cae2f2da86
midbenke <idmossab@gmail.com>
2024-10-27 13:39:51 +0100

    handler id empty

--- handlersMethodes.go
+++ handlersMethodes.go
@@ -9,13 +9,24 @@ func getHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		artists := fetchData[[]Artist](ArtistsURL)
 		renderTemplate(w, "index.html", &artists)
-	}else{
-		errorHandler(w,http.StatusNotFound)
+	} else {
+		errorHandler(w, http.StatusNotFound)
 	}
 }
 
 func getDetail(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	artist := fetchCompleteArtistData(id)
-	renderTemplate(w, "detail.html", &artist)
+	if r.Method != http.MethodGet {
+		errorHandler(w, http.StatusMethodNotAllowed)
+	}
+	if r.URL.Path == "/detail" {
+		id := r.URL.Query().Get("id")
+		if id == "" {
+			errorHandler(w, http.StatusBadRequest)
+			return
+		}
+		artist := fetchCompleteArtistData(id)
+		renderTemplate(w, "detail.html", &artist)
+	} else {
+		errorHandler(w, http.StatusNotFound)
+	}
 }
